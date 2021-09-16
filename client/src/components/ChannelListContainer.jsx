aa27b4efb46daac8fe813709f4ecfc5f4386e2de
JvoProgramming <jvoprogramming@gmail.com>
2021-09-16 13:25:38 -0700

    finished channel container

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -3,7 +3,8 @@ import { ChannelList, useChatContext } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
 
 import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './';
-import UCRIcon from '..assets/ucr.png';
+import UCRIcon from '../assets/ucr.jpg';
+import LogoutIcon from '../assets/logout.png';
 
 const SideBar = () => (
     <div className="channel-list__sidebar">
@@ -12,14 +13,28 @@ const SideBar = () => (
                 <img src={UCRIcon} alt="UCR"  width="30"/>
             </div>
         </div>
+        <div className="channel-list__sidebar__icon2">
+            <div className="icon1__inner">
+                <img src={LogoutIcon} alt="Logout"  width="30"/>
+            </div>
+        </div>
+    </div>
+)
+
+const CompanyHeader = () => (
+    <div className="channel-list__header">
+        <p className="channel-list__header__text">Schools</p>
     </div>
 )
 
 const ChannelListContainer = () => {
     return (
-        <div>
-            ChannelListContainer
-        </div>
+        <>
+            <SideBar />
+            <div className="channel-list__list__wrapper">
+                <CompanyHeader />
+            </div>
+        </>
     )
 }
 
